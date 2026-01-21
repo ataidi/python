0377215961f660f3c539d4663c2a1d47b6da3016
ataidi <ataiditamtam0@gmail.com>
2026-01-21 12:37:29 +0300

    my programming code

---
+++ ngoma.py
@@ -0,0 +1,11 @@
+print("hello Iam ngomas i sell those product en ligne")
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+print("alerte time")
+x, y, z = "10","12","14"
+print(x)
+print(y)
+print(z)
+
---
+++ python.py
@@ -0,0 +1,6 @@
+#Assigment & creating a variables called , weight and age , inpulize. the following values.
+name="john kevin"
+height=58.79
+weight=70.69
+age=34
+print(f"my name is {name}my age is {age} my height is {height} my weightis{weight}")
