98037bc43b3c4ede04d493c29fad3512371deb0b
ataidi <ataiditamtam0@gmail.com>
2026-01-21 11:17:01 +0300

    my python programm

---
+++ variables.py
@@ -0,0 +1,5 @@
+#declare available called customer ataidi,safaricom phone's,35000ksh,intialize the variable with some values and output the variables value.
+customer_name="ataidi"
+product="iphone"
+price="35000ksh"
+print(f"customer name is  {customer_name} product buget{product} & its price is {price}")
